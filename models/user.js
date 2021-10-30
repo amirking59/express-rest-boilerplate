6f18f8444f24381e45dd8051b47de3b6ab8ead6f
Amir <ah.rezayy@chmail.ir>
2021-10-30 20:10:43 +0330

    fix loggin db

--- models/user.js
+++ models/user.js
@@ -1,8 +1,6 @@
-const bcrypt = require('bcrypt');
 const {
     Model,
 } = require('sequelize');
-const db = require('.');
 
 module.exports = (sequelize, DataTypes) => {
     class User extends Model {
